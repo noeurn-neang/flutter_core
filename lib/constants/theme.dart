4418c29f583817c0c77b352f53f443dda693c19d
Neang <noeurnnneang@gmail.com>
2024-01-23 22:38:34 +0700

    add search debounce

--- lib/constants/theme.dart
+++ lib/constants/theme.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
 const Color disabledColor = Colors.grey;
-const Color dangerColor = Colors.redAccent;
+const Color dangerColor = Color.fromARGB(255, 230, 60, 60);
 const Color successColor = Color(0xff3ead53);
-const Color warningColor = Color.fromARGB(255, 184, 121, 12);
+const Color warningColor = Color.fromARGB(255, 234, 89, 17);
