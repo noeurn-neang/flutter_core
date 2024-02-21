87abeff072082424b1e5405c18b0f76558e54b86
Neang <noeurnnneang@gmail.com>
2024-02-21 11:33:11 +0700

    done

--- lib/constants/common.dart
+++ lib/constants/common.dart
@@ -22,5 +22,6 @@ var windowHeight = MediaQuery.of(Get.context!).size.height;
 var statusBarHeight = MediaQuery.of(Get.context!).viewPadding.top;
 var screenHeight = windowHeight - statusBarHeight;
 var screenWidth = MediaQuery.of(Get.context!).size.width;
+var isTablet = MediaQuery.of(Get.context!).size.shortestSide > 700;
 
 var curentDate = getCurrentDate();
