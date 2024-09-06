baa71e411e4a7c602c22df066c3bfc126f7ec17a
Noeurn Neang <noeurnnneang@gmail.com>
2024-09-06 10:05:06 +0700

    Change fixed font size

--- lib/styles/texts.dart
+++ lib/styles/texts.dart
@@ -2,9 +2,12 @@ import 'package:flutter/material.dart';
 
 import '../getx.dart';
 
-TextStyle titleSmallStyle = Theme.of(Get.context!).textTheme.titleSmall!;
-TextStyle titleMediumStyle = Theme.of(Get.context!).textTheme.titleMedium!;
-TextStyle titleLargeStyle = Theme.of(Get.context!).textTheme.titleLarge!;
+TextStyle titleSmallStyle =
+    Theme.of(Get.context!).textTheme.titleSmall!.copyWith(fontSize: 12);
+TextStyle titleMediumStyle =
+    Theme.of(Get.context!).textTheme.titleMedium!.copyWith(fontSize: 14);
+TextStyle titleLargeStyle =
+    Theme.of(Get.context!).textTheme.titleLarge!.copyWith(fontSize: 16);
 
 TextStyle formLabelStyle = Theme.of(Get.context!)
     .textTheme
